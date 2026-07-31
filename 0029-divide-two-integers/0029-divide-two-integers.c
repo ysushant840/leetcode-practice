5e187c6951d5bff9f5a937136ef19a0b450bb489
Sushant <sushantmasthai@gmail.com>
2026-07-31 22:27:41 +0530

    Time: 3 ms (5.39%), Space: 8.9 MB (89.32%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.c
@@ -0,0 +1,31 @@
+#include <limits.h>
+#include <stdlib.h>
+
+int divide(int dividend, int divisor) {
+    // Overflow case
+    if (dividend == INT_MIN && divisor == -1)
+        return INT_MAX;
+
+    long long dvd = llabs((long long)dividend);
+    long long dvs = llabs((long long)divisor);
+    long long ans = 0;
+
+    while (dvd >= dvs) {
+        long long temp = dvs;
+        long long multiple = 1;
+
+        while ((temp << 1) <= dvd) {
+            temp <<= 1;
+            multiple <<= 1;
+        }
+
+        dvd -= temp;
+        ans += multiple;
+    }
+
+    // Apply sign
+    if ((dividend < 0) ^ (divisor < 0))
+        ans = -ans;
+
+    return (int)ans;
+}
